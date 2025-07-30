39358647c921e037cca983cedece84556939cd8d
Jon Keam <jpkeam@gmail.com>
2025-07-30 12:50:54 -0400

    whitespace update

--- utils/services_client.go
+++ utils/services_client.go
@@ -10,7 +10,10 @@ import (
 // Returns new red hat operator marketplace client
 func NewOpenShiftRegistryClient() RegistryClient {
 	// open connection
-	conn, err := grpc.NewClient("redhat-operators.openshift-marketplace.svc.cluster.local:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	conn, err := grpc.NewClient(
+		"redhat-operators.openshift-marketplace.svc.cluster.local:50051",
+		grpc.WithTransportCredentials(insecure.NewCredentials()))
+
 	if err != nil {
 		log.Fatalf("Failed connecting: %v", err)
 	}
