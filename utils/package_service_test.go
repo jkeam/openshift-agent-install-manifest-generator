d855bddfdd366119f726f4cb8d02525e0847ffc0
Jon Keam <jpkeam@gmail.com>
2025-08-01 09:47:06 -0400

    remove dead test code

--- utils/package_service_test.go
+++ utils/package_service_test.go
@@ -2,17 +2,18 @@ package utils
 
 import (
 	context "context"
-	"testing"
 	"fmt"
 	"io"
+	"testing"
 
-	grpc "google.golang.org/grpc"
 	gomock "go.uber.org/mock/gomock"
+	grpc "google.golang.org/grpc"
 )
 
 type ServerStreamingClientMock struct {
 	grpc.ClientStream
 }
+
 func (s *ServerStreamingClientMock) Recv() (*PackageName, error) {
 	return &PackageName{Name: "submariner"}, io.EOF
 }
@@ -24,19 +25,19 @@ func (o *OpenShiftRegistryClientMock) ListPackages(ctx context.Context, in *List
 	return nil, nil
 }
 func (o *OpenShiftRegistryClientMock) GetPackage(ctx context.Context, in *GetPackageRequest, _ ...grpc.CallOption) (*Package, error) {
-	channels := []*Channel {
+	channels := []*Channel{
 		{Name: "channel", CsvName: "csv-name"},
 	}
 	thePackage := &Package{
-		Name: in.Name,
+		Name:               in.Name,
 		DefaultChannelName: in.Name,
-		Channels: channels,
+		Channels:           channels,
 	}
 
 	return thePackage, nil
 }
 func (o *OpenShiftRegistryClientMock) GetBundle(ctx context.Context, in *GetBundleRequest, _ ...grpc.CallOption) (*Bundle, error) {
-	bundle := &Bundle {
+	bundle := &Bundle{
 		CsvJson: fmt.Sprintf(`{"spec": {displayName: "%s", "relatedImages": []}}`, in.PkgName),
 	}
 	return bundle, nil
@@ -45,32 +46,13 @@ func (o *OpenShiftRegistryClientMock) GetBundle(ctx context.Context, in *GetBund
 func TestGetPackageByName(t *testing.T) {
 	client := &OpenShiftRegistryClientMock{}
 	GetPackageByName(client, "test")
-	a := 1 + 3
-	if a != 4 {
-		t.Errorf("Math is broken")
-	}
 }
 
 func TestGetPackages(t *testing.T) {
 	ctrl := gomock.NewController(t)
-	/*
-	channels := []*Channel {
-		{Name: "channel", CsvName: "csv-name"},
-	}
-	thePackage := &Package{
-		Name: "submariner",
-		DefaultChannelName: "submariner",
-		Channels: channels,
-	}
-		*/
 	var serverStreamingClient grpc.ServerStreamingClient[PackageName]
-	serverStreamingClient = &ServerStreamingClientMock {}
+	serverStreamingClient = &ServerStreamingClientMock{}
 	mockClient := NewMockRegistryClient(ctrl)
 	mockClient.EXPECT().ListPackages(context.Background(), &ListPackageRequest{}).Return(serverStreamingClient, nil).MinTimes(1)
 	GetPackages(mockClient)
-    //mockClient.AssertExpectations(t)
-	a := 1 + 3
-	if a != 4 {
-		t.Errorf("Math is broken")
-	}
 }
